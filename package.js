c0db22e67357ac102079ce34829981fe04148d11
mariapesm <youandmecanyousee@hotmail.com>
2018-12-27 23:10:00 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,33 @@
+{
+  "name": "fcc-bookflow",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "nodemon index.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/antipasjiajia/fcc-bookflow.git"
+  },
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/antipasjiajia/fcc-bookflow/issues"
+  },
+  "homepage": "https://github.com/antipasjiajia/fcc-bookflow#readme",
+  "dependencies": {
+    "body-parser": "^1.17.2",
+    "cookie-parser": "^1.4.3",
+    "express": "^4.15.3",
+    "jsonwebtoken": "^7.4.1",
+    "mongoose": "^4.11.3",
+    "node-fetch": "^1.7.1",
+    "passport": "^0.3.2",
+    "passport-local-mongoose": "^4.0.0",
+    "serve-favicon": "^2.4.3"
+  },
+  "devDependencies": {
+    "nodemon": "^1.11.0"
+  }
+}
