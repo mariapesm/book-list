80da801a2b632bb085d8e1c17bf33b902c01b72c
mariapesm <youandmecanyousee@hotmail.com>
2018-12-28 22:52:53 +0200

    Delete auth.js

--- auth.js
+++
@@ -1,7 +0,0 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var Users = require('./models/users');
-
-exports.local = passport.use(new LocalStrategy(Users.authenticate()));
-passport.serializeUser(Users.serializeUser());
-passport.deserializeUser(Users.deserializeUser());
